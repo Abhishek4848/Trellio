06a01051affe7a5cb8ac6f984d918c230f550fd5
Abhishek4848 <54174670+Abhishek4848@users.noreply.github.com>
2020-11-11 01:26:06 +0530

    Minor fixes

--- src/components/screens/NavBar.js
+++ src/components/screens/NavBar.js
@@ -2,14 +2,10 @@ import React from "react";
 import { slide as Menu } from "react-burger-menu";
 import './NavBar.css';
 import {Link} from "react-router-dom";
-import Logo from "../layouts/images/icon.svg";
+
 export default props => {
   return (
     <Menu {...props}>
-      <span>
-      <img src = {Logo} height="40px" width="40px" alt="logo"></img>
-      <span id="icon">Trillio.</span>
-      </span>
       <a className="menu-item" href="/">
         My Profile
       </a>
