7e7fe01bc04ca3f6fb753b4fd29869e20c617c0d
Abhishek4848 <54174670+Abhishek4848@users.noreply.github.com>
2020-11-16 00:37:01 +0530

    Minor fixes

--- src/components/layouts/Login.js
+++ src/components/layouts/Login.js
@@ -10,7 +10,7 @@ export const Login = () => {
                 <input type="email" name="" placeholder="E-mail Addres"></input>
                 <input type="password" name="" placeholder="Password"></input>
                 <input type="submit" name="" value="Login"></input>
-                <p>don't have an account? <Link to="/Register">Register</Link></p>
+                <p>Don't have an account? <Link to="/Register">Register</Link></p>
             </form>
             <footer>
                     <Link to="/"><img src={logo} height="70px" width="70px" alt="icon"></img></Link>
