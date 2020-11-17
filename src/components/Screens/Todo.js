0504730040bc8a14f523ea915d5b1765922ca3ac
abhishek-pes <abhishekmis402gmail.com>
2020-11-17 19:37:10 +0530

    added backend for todo component

--- src/components/Screens/Todo.js
+++ src/components/Screens/Todo.js
@@ -1,71 +1,120 @@
 import React, { useState, useEffect } from 'react';
 import './Todo.css';
+import SideNav from "./SideNav";
+
 
 
 function Todo(){
-    const [todoData, setTodoData] = useState([])
-    const [textInput, setTextInput] = useState("")
+    const [data,setdata] = useState([])
+    const [task, setTextInput] = useState("")
     const [error, setError] = useState(false)
 
-    const addItem = (e) =>{
-        e.preventDefault();
-        if (error) return;
-        const tempData = [...todoData];
-        tempData.push(textInput)
-        setTodoData(tempData)
-        setTextInput("")
-    }
+    function getTask()
+    {
+    fetch("http://localhost:5000/api/todo/tasks",{
+        method:"get",
+        headers:{
+            "x-auth-token":localStorage.getItem('token')
+        }
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        setdata(res)
+    })
+}
+    useEffect(() => {
+        
+    getTask();
+        // eslint-disable-next-line
+    },[]);
+
+
+
+
+
+    const submitHandler = (event) => {
+        try {
+          event.preventDefault();
+    
+          fetch("http://localhost:5000/api/todo/", {
+            method: "post",
+    
+            body: JSON.stringify({
+              task,
+            }),
+            headers: {
+              "Content-type": "application/json",
+              "x-auth-token": localStorage.getItem("token"),
+            },
+          })
+            .then((res) => res.json())
+            .then((res) => {
+            //   console.log(res)
+              getTask();
+            });
+        } catch (err) {}
+      };
+
 
     //this hook displays a error message when the input characters exceed 25
     //Displays nothing if characters<25
     useEffect(() => {
-        if(textInput.length > 25)setError(true);
+        if(task.length > 50)setError(true);
         else setError(false)
-    },[textInput])
+    },[task])
     
-    const removeItem =(index) => {
-        let newData = [...todoData]
-        newData.splice(index,1)
-        setTodoData(newData)
-    }
+    // console.log(data)
+
+
+    const Completed = (tid) => {
+        try{
+            // console.log(tid)
+            const url = "http://localhost:5000/api/todo/delete/"+tid
+            fetch(url, {
     
-    const editItem = (index) => {
-        if(error) return;
-        let newData = [...todoData]
-        newData[index] = textInput;
-        setTodoData(newData)
+            method: "delete",
+            headers: {
+                "x-auth-token": localStorage.getItem('token')
+            }
+            })
+            .then(res => {
+                alert("Congrats! On completing your Task")
+                getTask();
+            })
+        }
+        catch(err){}
     }
 
     return(
         <div>
         <header className="header">
-            <h1>TODO</h1>
+            <h1>TODO</h1>        
         </header>
+        <SideNav/>
             <div className="inp1">
-            <form onSubmit={addItem}>
+            <form onSubmit={submitHandler}>
                 <label className="inputLabel1">
-                    Add Item:
-                    <input type="text" value = {textInput} onChange={(e) =>setTextInput(e.target.value)} placeholder="Enter task" className="inputfield1"/>
+                    Add Your Task:
+                    <input type="text" onChange={(e) =>setTextInput(e.target.value)} placeholder="Enter task" className="inputfield1"/>
                 </label>
-                <input type = "submit" value = "submit" className="submitbtn1"/>    
+                <input type = "submit" value = "Add" className="submitbtn1"/>    
             </form>
             </div>
             {error ? alert("Enter a short task name !!"): null}
             {
-                 todoData.map((item,index) =>{
+                 data.map((response) =>{
                      return(
-                    <section class="card-list1">
-                        <article class="card1" key={index}>
-                          <header class="card-header1">
-                            <p className="date">{new Date().toLocaleString()}
+                    <section className="card-list1" key={response._id}>
+                        <article className="card1" >
+                          <header className="card-header1">
+                            <p className="date">{response.date}
                             </p>
-                            <h2 className="item">{item}</h2>
+                            <h2 className="item">{response.task}</h2>
                           </header>
                             <p>Author</p>
-                            <p>Jeff Delaney</p>
-                          <div class="tags1">
-                            <button onClick ={()=>editItem(index)} className="editbtn1">Edit</button>
-                            <button onClick={() => removeItem(index)} className="deletebtn1">Delete</button>
+                            <p>{response.user.name}</p>
+                          <div className="tags1">
+                            <button  className="deletebtn1" onClick = {() => Completed(response._id)}>Completed</button>
                           </div>
                         </article>
                     </section>
